908a5cd7efa26b2e9b0153be8abb4521bf7435ae
AndreRicardoSc <andrenarutto7@gmail.com>
2026-03-09 19:39:17 -0300

    Mudando o AppHeader

--- src/components/layouts/AppHeader.vue
+++ src/components/layouts/AppHeader.vue
@@ -1,37 +1,126 @@
-<script>
+<script setup>
+import { useDarkLightMode } from "../../composables/useDarkLightMode";
+import { useRouter } from "vue-router";
+const { mode, setLight, setDark } = useDarkLightMode();
+const router = useRouter();
+
+const props = defineProps({
+  title: String,
+  subtitle: {
+    type: String,
+    default: "",
+  },
+  buttonBack: {
+    type: Boolean,
+    default: false,
+  },
+});
 </script>
 <template>
-    <header>
-   
-        <span class="mdi mdi-arrow-left-drop-circle"></span>
-        <div>
-        <h1>Calculadora de Gorjetas e Divisão</h1>
-        <p>Seus Calculos Salvos</p>
-        </div>
-    </header>
+  <header :class="mode === 'light' ? 'header-light' : 'header-dark'">
+    <div>
+      <span
+        v-if="props.buttonBack"
+        class="mdi mdi-arrow-left-thin"
+        :class="mode === 'light' ? 'back-light' : 'back-dark'"
+        @click="router.back()"
+      ></span>
+      <span v-else class="mdi mdi-calculator home"></span>
+      <div class="titles">
+        <h1 :class="mode === 'light' ? 'text-black' : 'text-white'">
+          {{ props.title }}
+        </h1>
+        <p class="subtitle" v-if="props.subtitle">{{ props.subtitle }}</p>
+      </div>
+    </div>
+    <button v-if="mode === 'light'" class="button-light" @click="setDark">
+        <span class="mdi mdi-weather-night"></span>
+    </button>
+    <button v-if="mode ==='dark'" class="button-dark" @click="setLight">
+        <span class="mdi mdi-lightbulb-on-30"></span>
+    </button>
+  </header>
 </template>
+
 <style scoped>
-    header{
-        background-color: #fff;
-        display: flex;
-        align-items: center;
-        padding-inline:30px ;
-        border-bottom-left-radius: 10px;
-        border-bottom-right-radius: 10px;
-    }
-    h1{
-            font-size: 25px;
-            font-family : 'Times New Roman', Times, serif;
-            color : #000;
+.header-light {
+  background-color: #fff;
+  padding: 30px;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  border-bottom: 1px solid #e5e7eb;
+}
 
-        }
-       span{
-         background-image: linear-gradient(to right, #780088, #f8037e );
-        -webkit-background-clip: text;
-        background-clip: text;
-        margin: 0 20px 0 0;
-        font-size: 40px;
-         -webkit-text-fill-color: transparent;
-       }
+.header-dark {
+  background-color: #1e2939;
+  padding: 30px;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  border-bottom: 1px solid #364153;
+}
 
+div {
+  display: flex;
+  align-items: center;
+  gap: 10px;
+}
+
+.back-light {
+  border: solid 2px #e5e7eb;
+  color: #364153;
+  font-size: 20px;
+  border-radius: 16px;
+  padding: 0 8px;
+}
+.back-dark {
+  border: solid 2px #364153;
+  color: #e5e7eb;
+  font-size: 20px;
+  border-radius: 16px;
+  padding: 0 8px;
+}
+
+.home {
+  font-size: 30px;
+  color: #ffffff;
+  border-radius: 16px;
+  padding: 2px 8px;
+  background: linear-gradient(
+    135deg,
+    var(--pink-primary),
+    var(--pink-secondary)
+  );
+}
+
+.titles {
+  display: block;
+}
+
+h1 {
+  font-size: 20px;
+}
+
+.subtitle {
+  color: #6a7282;
+  font-size: 15px;
+}
+
+.button-light {
+    color: #9810fa;
+    border: solid 2px #e5e7eb;
+    border-radius: 16px;
+    padding: 5px 10px;
+    background: transparent;
+    font-size: 25px;
+}
+.button-dark {
+    color: #d08700;
+    border: solid 2px #364153;
+    border-radius: 16px;
+    padding: 5px 10px;
+    background: transparent;
+    font-size: 25px;
+}
 </style>
