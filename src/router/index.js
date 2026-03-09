cf4e393d13fc965284bb8138c965f422e2a869c3
AndreRicardoSc <andrenarutto7@gmail.com>
2026-03-09 18:38:12 -0300

    Adicionando nome na home

--- src/router/index.js
+++ src/router/index.js
@@ -3,6 +3,7 @@ import { createRouter, createWebHistory } from "vue-router";
 const routes = [
     {
         path: '/',
+        name: 'home-view'
         component: () => import('../views/HomeView.vue')
     }
 ];
