d44c34c471033dd338a56b8600115b70a8c90373
AndreRicardoSc <andrenarutto7@gmail.com>
2026-03-09 18:01:25 -0300

    DespesaDetailView criada

--- src/router/index.js
+++ src/router/index.js
@@ -4,6 +4,13 @@ const routes = [
     {
         path: '/',
         component: () => import('../views/HomeView.vue')
+    },
+    // {
+    //     path: 'despesas/:id/edit',
+    // },
+    {
+        path: '/despesas/:id',
+        component: () => import('../views/DespesaDetailView.vue'),
     }
 ];
 
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,4 +1,15 @@
-<script setup></script>
+<script setup>
+    import { onMounted } from 'vue';
+    import { useDespesas } from '../composables/useDespesas';
+    const { despesas, adicionarDespesa } = useDespesas();
+
+    onMounted(() => {
+
+    })
+</script>
 <template>
     <h1>Home</h1>
+    <div v-for="despesa in despesas">
+        {{ despesa }}
+    </div>
 </template>
