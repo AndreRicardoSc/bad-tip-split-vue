6b305642479eb300fbec60dd0cc7479a410f13e3
AndreRicardoSc <andrenarutto7@gmail.com>
2026-03-09 19:39:35 -0300

    Mudando o AppHeader

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,59 +1,32 @@
 <script setup>
-import AppHeader from '../components/layouts/AppHeader.vue';
-import DespesaCard from '../components/cards/DespesaCard.vue';
-import { onMounted, ref } from 'vue';
-
-    const despesas = ref([
-        {
-            titulo: 'Churrasco',
-            total: 200,
-            gorjeta: 20,
-            porPessoa: 44,
-        },
-        {
-            titulo: 'Churrasco',
-            total: 200,
-            gorjeta: 20,
-            porPessoa: 44,
-        },
-        {
-            titulo: 'Churrasco',
-            total: 200,
-            gorjeta: 20,
-            porPessoa: 44,
-        },
-       
-    ]);
-
-    function pegarDespesas() {
-        const response = localStorage.getItem("despesas");
-        if(response) {
-            despesas.value = response
-        }
-    }
-
-    onMounted(pegarDespesas)
+import AppHeader from "../components/layouts/AppHeader.vue";
+import DespesaCard from "../components/cards/DespesaCard.vue";
+import { onMounted, ref } from "vue";
+import { useDespesas } from "../composables/useDespesas";
+const { despesas } = useDespesas();
 </script>
 <template>
-<AppHeader/>
-<div class="despesas">
-
-  <div class="cards">
-    <DespesaCard v-for="(despesa, index) of despesas" :key="index" :titulo="despesa.titulo" :total="despesa.total" :gorjeta="despesa.gorjeta" :porPessoa="despesa.porPessoa"/>
+  <AppHeader title="Divisão de Despesas" subtitle="Suas despesas salvas"/>
+  <div class="despesas">
+    <div class="cards">
+      <DespesaCard
+        v-for="(despesa, index) of despesas"
+        :key="index"
+        :titulo="despesa.titulo"
+        :total="despesa.total"
+        :gorjeta="despesa.gorjeta"
+        :porPessoa="despesa.porPessoa"
+      />
+    </div>
   </div>
-</div>
 </template>
 <style scoped>
-
-.despesas{
-    padding: 20px;
-    
+.despesas {
+  padding: 20px;
 }
 .cards {
-    display: flex;
-    flex-direction: column;
-    gap: 20px;
+  display: flex;
+  flex-direction: column;
+  gap: 20px;
 }
-
-
 </style>
